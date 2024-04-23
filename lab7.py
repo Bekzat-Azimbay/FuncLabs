203ff0029652df83594d513947bbfa3db9e71b38
Bekzat <azimbaybekzat7@gmail.com>
2024-04-23 18:54:02 +0600

    add

---
+++ lab7.py
@@ -0,0 +1,16 @@
+# consumers.py
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from myapp.tasks import process_event
+
+class EventConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        # Dispatch Celery task to handle the event asynchronously
+        process_event.delay(data)
